1f6d8ee29c639183322bf15062852a787fab43f7
John Esco <47394267+AzianEquation@users.noreply.github.com>
2019-10-13 19:15:20 -0400

    Added Second empty Activity which starts on a button press

--- app/src/main/java/com/example/csce490m3research/MainActivity.java
+++ app/src/main/java/com/example/csce490m3research/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.csce490m3research;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String EXTRA_MESSAGE = "com.example.CSCE490M3Research.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        // Do something in response to button
+        // create new intent object (Context, Class) this activity is subclass of context. Class is where system delivers Intent
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // this finds the plain text named editText
+        EditText editText = (EditText) findViewById(R.id.editText);
+        // retrieve text that was entered
+        String message = editText.getText().toString();
+        // put extra add the value of EditText to the intent. Intent can carry data types as key/val pairs called extras
+        intent.putExtra(EXTRA_MESSAGE,message);
+        startActivity(intent);
     }
 }
