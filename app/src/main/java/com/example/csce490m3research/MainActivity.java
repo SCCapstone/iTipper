cc2bf18a9311a3a7b62baa38e05ef87a244ab428
Tyler Chambers <49771267+tylerpchambers@users.noreply.github.com>
2019-12-02 20:19:36 -0500

    Separated activities into screens (successfully this time)

--- app/src/main/java/com/example/csce490m3research/MainActivity.java
+++ app/src/main/java/com/example/csce490m3research/MainActivity.java
@@ -1,33 +1,23 @@
-// coded by John Esco, iTipper
 package com.example.csce490m3research;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String EXTRA_MESSAGE = "com.example.CSCE490M3Research.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    // redirects to google maps on button press
-    public void sendMessage(View view) {
-        // Do something in response to button
-        // create new intent object (Context, Class) this activity is subclass of context. Class is where system delivers Intent
-        //Intent intent = new Intent(this, DisplayMessageActivity.class);
-        Intent intent = new Intent(this, MapsActivity.class);
-        // this finds the plain text named editText
-        EditText editText = (EditText) findViewById(R.id.editText);
-        // retrieve text that was entered
-        String message = editText.getText().toString();
-        // put extra add the value of EditText to the intent. Intent can carry data types as key/val pairs called extras
-        intent.putExtra(EXTRA_MESSAGE,message);
-        startActivity(intent);
+
+    // Called when user taps "login" button
+    public void login(View view) {
+        // Send user to main menu screen
+        Intent mainMenu = new Intent(this, MainMenuActivity.class);
+        startActivity(mainMenu);
     }
 }
