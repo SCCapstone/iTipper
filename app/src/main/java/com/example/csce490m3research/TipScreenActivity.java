397496307c6aa1ea4da111b85a1ed992e529e06a
Paolo Milan <milan.paolo@yahoo.com>
2019-12-04 18:20:59 -0500

    Name change

--- app/src/main/java/com/example/csce490m3research/TipScreenActivity.java
+++ app/src/main/java/com/example/csce490m3research/TipScreenActivity.java
@@ -8,14 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class TipScreenActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.CSCE490M3Research.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tip_screen);
     }
     // redirects to google maps on button press
     public void sendMessage(View view) {
