998963c9fbb1b8b82b66235f2b682966d21cd3cd
Kevin Zheng <42kevinzheng@gmail.com>
2019-12-06 19:33:52 -0500

    Add files via upload

--- app/src/main/java/com/example/csce490m3research/LoginActivity.java
+++ app/src/main/java/com/example/csce490m3research/LoginActivity.java
@@ -1,56 +1,109 @@
 package com.example.csce490m3research;
 
-
-import android.app.Activity;
-import android.graphics.Color;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-    Button button;
-    EditText username, password;
+
+    private EditText UserEmail, UserPassword;
+    private Button btnSignIn;
+    private ProgressDialog loadingBar;
+    private FirebaseAuth mFirebaseAuth;
+    private TextView NeedNewAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        UserEmail = findViewById(R.id.Username);
+        UserPassword = findViewById(R.id.User_Password);
+        btnSignIn = findViewById(R.id.login_button);
+        NeedNewAccountLink = findViewById(R.id.register_account_link);
+        loadingBar = new ProgressDialog(this);
 
-        button = (Button) findViewById(R.id.button);
-        username = (EditText) findViewById(R.id.user);
-        password = (EditText) findViewById(R.id.password);
 
-        final Intent mainMenu = new Intent(this, MainMenuActivity.class);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                AllowingUserToLogin();
+            }
+        });
 
-        button.setOnClickListener(new View.OnClickListener() {
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (username.getText().toString().equals("admin") &&
-                        password.getText().toString().equals("admin")) {
-                    Toast.makeText(getApplicationContext(),
-                            "Redirecting...", Toast.LENGTH_SHORT).show();
-
-                    startActivity(mainMenu);
-
-                }
+                Intent intSignUp = new Intent(LoginActivity.this, Register.class);
+                startActivity(intSignUp);
             }
         });
     }
 
-    public void gotoMainMenu(View view) {
-        Intent mainMenu = new Intent(this, MapsDialogueActivity.class);
-        startActivity(mainMenu);
+    private void AllowingUserToLogin() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please write your email...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Login");
+            loadingBar.setMessage("Please wait, while we are allowing you to login into your Account...");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+            mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                SendUserToMainActivity();
+                                Toast.makeText(LoginActivity.this, "you are Logged In successfully.", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else
+                            {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, "Error occured: " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
     }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(LoginActivity.this, MainMenuActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+
+
 }
