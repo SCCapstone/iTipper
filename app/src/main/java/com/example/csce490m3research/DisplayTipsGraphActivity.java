dc1dc7dc75cc6eb6a302f7663f4108e279a0825b
John Esco <47394267+AzianEquation@users.noreply.github.com>
2020-03-25 17:15:33 -0400

    Fixed the graph such that the color matches the app's primary color. Changed the graph to a bar graph and fixed the title, and axis labels. From mainActivity, the button goes directly to the graph such that the example graph is not shown. Added view graph button to the tip entry activity

--- app/src/main/java/com/example/csce490m3research/DisplayTipsGraphActivity.java
+++ app/src/main/java/com/example/csce490m3research/DisplayTipsGraphActivity.java
@@ -83,9 +83,12 @@ public class DisplayTipsGraphActivity extends Activity {
                 //domainLabels = new Number[listSize];
                 //rangeLabels = new Number[listSize];
                 // populate with values
+
                 for (int i = 0; i < listSize; i++) {
                     domainLabels[i] = i + 1;
                 }
+
+
                 // iterate through list and capture tip values
                 int counter = 0;
                 for (Object t : tipsList) {
@@ -110,11 +113,10 @@ public class DisplayTipsGraphActivity extends Activity {
                 Log.d("Value 6: ", rangeLabels[5].toString());
                 setContentView(R.layout.activity_display_graph);
                 plot = findViewById(R.id.plot);
+                // set bounds to the plot
+                plot.setRangeLowerBoundary(0, BoundaryMode.FIXED);
+                plot.setRangeUpperBoundary(100,BoundaryMode.AUTO);
                 // create a couple arrays of y-values to plot:
-              //  final Number[] domainLabels = {1, 2, 3, 6, 7, 8, 9, 10, 13, 14};
-              //  Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
-
-                // turn the above arrays into XYSeries':
                 // (Y_VALS_ONLY means use the element index as the x value)
                 XYSeries series1 = new SimpleXYSeries(
                         Arrays.asList(rangeLabels), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Tips");
@@ -122,14 +124,18 @@ public class DisplayTipsGraphActivity extends Activity {
                 // and configure them from xml:
                 LineAndPointFormatter series1Format =
                         new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
-
-
+                BarFormatter bf = new BarFormatter(Color.GREEN, Color.BLACK);
+                BarRenderer renderer = plot.getRenderer(BarRenderer.class);
+                // increment each axis by 1
+                plot.setDomainStep(StepMode.INCREMENT_BY_VAL, 1.0);
+                plot.setRangeStep(StepMode.INCREMENT_BY_VAL, 1.0);
                 // just for fun, add some smoothing to the lines:
-                // see: http://androidplot.com/smooth-curves-and-androidplot/
-                series1Format.setInterpolationParams(
-                        new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+                //series1Format.setInterpolationParams(
+                       // new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
                 // add a new series' to the xyplot:
-                plot.addSeries(series1, series1Format);
+                //plot.addSeries(series1, series1Format);
+                plot.addSeries(series1, bf);
+                /*
                 plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
                     @Override
                     public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
@@ -142,6 +148,9 @@ public class DisplayTipsGraphActivity extends Activity {
                     }
                 });
 
+                 */
+
+
 
             }
         });
@@ -149,100 +158,4 @@ public class DisplayTipsGraphActivity extends Activity {
 
 
     }
-    public void printGraph(Number[] list) {
-        plot = findViewById(R.id.plot);
-        // create a couple arrays of y-values to plot:
-        final Number[] domainLabels = {1, 2, 3, 6, 7, 8, 9, 10, 13, 14};
-        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
-
-        // turn the above arrays into XYSeries':
-        // (Y_VALS_ONLY means use the element index as the x value)
-        XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(list), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
-        // create formatters to use for drawing a series using LineAndPointRenderer
-        // and configure them from xml:
-        LineAndPointFormatter series1Format =
-                new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
-
-
-        // just for fun, add some smoothing to the lines:
-        // see: http://androidplot.com/smooth-curves-and-androidplot/
-        series1Format.setInterpolationParams(
-                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format);
-        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
-            @Override
-            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-                int i = Math.round(((Number) obj).floatValue());
-                return toAppendTo.append(domainLabels[i]);
-            }
-            @Override
-            public Object parseObject(String source, ParsePosition pos) {
-                return null;
-            }
-        });
-
-    }
-
-    public void printDefaultGraph(Number[] list) {
-
-        // create a couple arrays of y-values to plot:
-        final Number[] domainLabels = {1, 2, 3, 6, 7, 8, 9, 10, 13, 14};
-        Number[] series1Numbers = {1, 4, 2, 8, 4, 16, 8, 32, 16, 64};
-        Number[] series2Numbers = {5, 2, 10, 5, 20, 10, 40, 20, 80, 40};
-
-        // turn the above arrays into XYSeries':
-        // (Y_VALS_ONLY means use the element index as the x value)
-        XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1");
-        XYSeries series2 = new SimpleXYSeries(
-                Arrays.asList(series2Numbers), SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2");
-
-        // create formatters to use for drawing a series using LineAndPointRenderer
-        // and configure them from xml:
-        LineAndPointFormatter series1Format =
-                new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
-
-        LineAndPointFormatter series2Format =
-                new LineAndPointFormatter(Color.RED, Color.GREEN, Color.BLUE, null);
-
-        // add an "dash" effect to the series2 line:
-        series2Format.getLinePaint().setPathEffect(new DashPathEffect(new float[] {
-
-                // always use DP when specifying pixel sizes, to keep things consistent across devices:
-                PixelUtils.dpToPix(20),
-                PixelUtils.dpToPix(15)}, 0));
-
-        // just for fun, add some smoothing to the lines:
-        // see: http://androidplot.com/smooth-curves-and-androidplot/
-        series1Format.setInterpolationParams(
-                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
-
-        series2Format.setInterpolationParams(
-                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
-
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format);
-        plot.addSeries(series2, series2Format);
-
-        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
-            @Override
-            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
-                int i = Math.round(((Number) obj).floatValue());
-                return toAppendTo.append(domainLabels[i]);
-            }
-            @Override
-            public Object parseObject(String source, ParsePosition pos) {
-                return null;
-            }
-        });
-
-    }
-
-
-
-
-
-
 }
