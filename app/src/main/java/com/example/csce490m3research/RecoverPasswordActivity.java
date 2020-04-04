53e70b082bfc285ccfbae23e1d36e51840e2bddb
Tyler Chambers <49771267+tylerpchambers@users.noreply.github.com>
2020-04-04 17:39:02 -0400

    Added password recovery to login screen.

---
+++ app/src/main/java/com/example/csce490m3research/RecoverPasswordActivity.java
@@ -0,0 +1,56 @@
+package com.example.csce490m3research;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RecoverPasswordActivity extends AppCompatActivity {
+    private EditText recoveryEmail;
+    private Button sendEmailButton;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recover_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        recoveryEmail = (EditText) findViewById(R.id.recovery_email);
+        sendEmailButton = (Button) findViewById(R.id.send_recovery_email_button);
+
+        final Context context = this;
+
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = recoveryEmail.getText().toString();
+
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(context, "Email sent.", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(context, LoginActivity.class));
+                        }
+                        else {
+                            String error = task.getException().getMessage();
+                            Toast.makeText(context, error, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
