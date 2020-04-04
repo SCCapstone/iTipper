ad015aa398db2434bd9b56d2abe5bf2569dd4f3b
TylerPChambers <tyler.p.chambers@gmail.com>
2020-04-03 23:47:52 -0400

    Made it so user authentication persists after app is closed. Closes #67

--- app/src/main/java/com/example/csce490m3research/HomeFragment.java
+++ app/src/main/java/com/example/csce490m3research/HomeFragment.java
@@ -1,18 +1,40 @@
 package com.example.csce490m3research;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+    private Button viewUserActivityButton;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return  inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        /** Add a listener to the "View your activity" to go to the ShiftLiftActivity
+         * when clicked
+         */
+        viewUserActivityButton = (Button) getView().findViewById(R.id.view_activity_button);
+        viewUserActivityButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), ShiftListActivity.class));
+            }
+        });
     }
 }
