d7fd7f8289a6a663da836b50410252ca6a2d8f98
TylerPChambers <tyler.p.chambers@gmail.com>
2020-04-21 22:49:58 -0400

    Shit screen UI changes
    * Made quick add buttons bigger.
    * Made it more clear that tapping a button twice quickly enters two tips.
    * Made fonts bigger.
    * Grouped shift info and start/end shift buttons.
    * Removed a lot of redundant buttons.
    * Entering a tip now displays a temporary message instead of permanent
    text.
    * Added some additional error handling to fix a crash when just putting
    a . in the custom tip box.
    
    Overall, the screen should be less cluttered now.

--- app/src/main/java/com/example/csce490m3research/EnterTipScreenActivity.java
+++ app/src/main/java/com/example/csce490m3research/EnterTipScreenActivity.java
@@ -1,7 +1,3 @@
-/** Screen where the user can input tips.
- *
- *  Authors: Paolo Milan, Tyler Chambers
- */
 package com.example.csce490m3research;
 
 import androidx.annotation.NonNull;
@@ -10,31 +6,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import org.w3c.dom.Text;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
-
+/** Screen where the user can input tips.
+ *
+ *  Authors: Paolo Milan, Tyler Chambers
+ */
 public class EnterTipScreenActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     TextView responseText;
-    TextView shiftText;
+    TextView toastText;
+    Toast toast;
+    static final int TOAST_TEXT_SIZE = 24;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +57,12 @@ public class EnterTipScreenActivity extends AppCompatActivity {
         final Button startShift = findViewById(R.id.startShiftButton);
         final Button endShift = findViewById(R.id.endShiftButton);
 
-        responseText = (TextView)findViewById(R.id.responseTextView);
-        shiftText = (TextView)findViewById(R.id.shift_text);
+        responseText = findViewById(R.id.responseTextView);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Shift Screen");
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Shift Screen");
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -100,9 +107,10 @@ public class EnterTipScreenActivity extends AppCompatActivity {
                     endShift.setAlpha(.5f);
                     endShift.setClickable(false);
                 }
-                shiftText.setText(shiftInfo);
+                responseText.setText(shiftInfo);
             }
         });
+
     }
 
     /** Called when the user taps the + button */
@@ -111,17 +119,22 @@ public class EnterTipScreenActivity extends AppCompatActivity {
         EditText editText = findViewById(R.id.tipInputText);
         String message = editText.getText().toString();
 
+        // Clear the user's input after the + button has been pushed
+        editText.setText("");
+
         try {
             Tip tip = new Tip(message);
             System.out.println(tip + " from user input");
 
             Database.writeTip(message);
 
-            editText.setText("");
-            responseText.setText("Entered tip: " + tip.toString());
+            displayTip("" + tip.getValue());
 
         } catch (InvalidTipException e) {
             String errorText = e.getMessage();
+            responseText.setText(errorText);
+        } catch (NumberFormatException e) {
+            String errorText = "Error: you put in something that wasn't a number.";
             responseText.setText(errorText);
         }
     }
@@ -136,12 +149,14 @@ public class EnterTipScreenActivity extends AppCompatActivity {
 
             Database.writeTip(tip);
 
-            responseText.setText("Entered tip: " + tip.toString());
+            displayTip(tipValue);
 
         } catch (InvalidTipException e) {
             String errorText = "Error entering tip: " + tipValue
                     + "\n" + "Tip should be greater than zero.";
             responseText.setText(errorText);
+        } catch (NumberFormatException e) {
+            responseText.setText(e.getMessage());
         }
     }
 
@@ -180,7 +195,7 @@ public class EnterTipScreenActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
+                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
                         document.getReference().update("end", Timestamp.now());
 
                         // Refresh the activity
@@ -213,6 +228,43 @@ public class EnterTipScreenActivity extends AppCompatActivity {
         startActivity(gotoMapsView);
     }
 
+    /**
+     * Display a temporary message on the screen to communicate to the user that their tip
+     * has been entered.
+     * If there's already a message being display, append the message to that message view.
+     * @param tip The value of the tip added.
+     */
+    public void displayTip(String tip) {
+        // There's no message currently displayed, so just display the message like normal.
+        if (toast == null) {
+            String msg = "Added tip: " + tip;
+            toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+
+            ViewGroup group = (ViewGroup) toast.getView();
+
+            TextView messageTextView = (TextView) group.getChildAt(0);
+            messageTextView.setTextSize(TOAST_TEXT_SIZE);
+
+            toast.show();
+        }
+        // If a message is already displayed, append to that message instead of making a new one.
+        else {
+            String msg = ((TextView) ((LinearLayout) toast.getView()).getChildAt(0))
+                    .getText().toString();
+            msg += ", " + tip;
+
+            toast.cancel();
+
+            toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+
+            ViewGroup group = (ViewGroup) toast.getView();
+
+            TextView messageTextView = (TextView) group.getChildAt(0);
+            messageTextView.setTextSize(TOAST_TEXT_SIZE);
+
+            toast.show();
+        }
+    }
 
 
 }
