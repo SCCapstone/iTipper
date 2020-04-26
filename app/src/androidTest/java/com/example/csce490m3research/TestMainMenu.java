cc591108c3036881670228ba80b35113dd4ad307
TylerPChambers <tyler.p.chambers@gmail.com>
2020-04-26 15:55:54 -0400

    Added some more tests
    * UID fetching
    * Navigation on main menu

--- app/src/androidTest/java/com/example/csce490m3research/TestMainMenu.java
+++ app/src/androidTest/java/com/example/csce490m3research/TestMainMenu.java
@@ -4,10 +4,13 @@ import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.*;
 import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static org.junit.Assert.*;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -34,5 +37,22 @@ public class TestMainMenu {
     public void isDisplayed() {
         onView(withId(R.id.welcomeMessage)).check(matches(ViewMatchers.isDisplayed()));
         onView(withId(R.id.view_all_tips_button)).check(matches(ViewMatchers.isDisplayed()));
+    }
+
+    @Test
+    public void getUid() {
+        String uid = Database.getUID();
+        String uid2 = FirebaseAuth.getInstance().getUid();
+
+        assertEquals(uid, uid2);
+    }
+
+    @Test
+    public void changeFragments() {
+        onView(withId(R.id.nav_graph)).perform(click());
+        onView(withId(R.id.plot)).check(matches(ViewMatchers.isDisplayed()));
+
+        onView(withId(R.id.nav_settings)).perform(click());
+        onView(withId(R.id.action_settings)).check(matches(ViewMatchers.isDisplayed()));
     }
 }
